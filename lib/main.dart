6f921243f325f5955f5d8abaf209a29fa421189f
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-18 15:43:43 +0300

    add get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:act_hub/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
     );
   }
 }
