cd79dbc42852d8c6e2983aaf56e953967af48891
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-21 15:57:28 +0300

    add elevation to constants

--- lib/core/service/theme_service.dart
+++ lib/core/service/theme_service.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../theme/manager_light_theme.dart';
+
 class ThemeService {
   ThemeMode getThemeMode() => ThemeMode.light;
-  // ThemeData getThemeData() => managerTh();
+  ThemeData getThemeData() => managerLightThemeData();
 }
