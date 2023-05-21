c673a064f85e8ce65e3b8894dc3005c41087ed07
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-21 12:32:10 +0300

    add will pop scope method & edit splash screen

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:act_hub/core/resources/manager_colors.dart';
+import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(backgroundColor: ManagerColors.primaryColor,body:);
   }
 }
