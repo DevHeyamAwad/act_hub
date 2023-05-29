23938eb3e92058adbba2d3fff7705a861f20659a
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-30 02:01:10 +0300

    edit dio factory

--- lib/core/network/dio_factory.dart
+++ lib/core/network/dio_factory.dart
@@ -1,25 +1,56 @@
-import 'package:act_hub/config/constants.dart';
-import 'package:act_hub/config/dependency_injection.dart';
-import 'package:act_hub/core/storage/local/app_settings_shared_preferences.dart';
 import 'package:dio/dio.dart';
-
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import '../../config/constants.dart';
+import '../../config/dependency_injection.dart';
 import '../resources/manager_strings.dart';
+import '../storage/local/app_settings_shared_preferences.dart';
 
 class DioFactory {
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
+      instance<AppSettingsSharedPreferences>();
+
   Future<Dio> getDio() async {
     Dio dio = Dio();
-    final AppSettingsSharedPreferences appSettingsPreferences =
-        instance<AppSettingsSharedPreferences>();
     Map<String, String> headers = {
       ApiConstants.authorization:
-          getAuthorization(appSettingsPreferences.getToken())
+          getAuthorization(_appSettingsSharedPreferences.getToken()),
     };
+
     dio.options = BaseOptions(
-      baseUrl: Constants.baseUrl,
+      baseUrl: ApiConstants.baseUrl,
       headers: headers,
-      receiveTimeout: const Duration(seconds: Constants.timeOutDuration),
-      sendTimeout: const Duration(seconds: Constants.timeOutDuration),
+      sendTimeout: const Duration(
+        seconds: ApiConstants.sendTimeOutDuration,
+      ),
+      receiveTimeout: const Duration(
+        seconds: ApiConstants.receiveTimeOutDuration,
+      ),
     );
+
+    InterceptorsWrapper interceptorsWrapper = InterceptorsWrapper(onRequest:
+        (RequestOptions options, RequestInterceptorHandler handler) async {
+      options.headers[ApiConstants.authorization] = getAuthorization(
+        _appSettingsSharedPreferences.getToken(),
+      );
+      return handler.next(options);
+    });
+
+    dio.interceptors.add(interceptorsWrapper);
+
+    if (!kReleaseMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          responseBody: true,
+          requestBody: true,
+          responseHeader: true,
+          requestHeader: true,
+          request: true,
+        ),
+      );
+    }
+
+    return dio;
   }
 
   String getAuthorization(String token) {
