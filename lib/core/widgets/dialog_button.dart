ab36d679cedde6fc0a166365a493b5b97b83a2c2
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-06 14:47:32 +0300

    dialog button

---
+++ lib/core/widgets/dialog_button.dart
@@ -0,0 +1,25 @@
+import 'package:act_hub/core/resources/manager_sizes.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../resources/manager_colors.dart';
+import '../resources/manager_fonts.dart';
+import '../resources/manager_styles.dart';
+import 'main_button.dart';
+
+Widget dialogButton({
+  void Function()? onPressed,
+  required String message,
+}) {
+  return mainButton(
+    child: Text(
+      message,
+      style: getMediumTextStyle(
+        fontSize: ManagerFontSize.s16,
+        color: ManagerColors.white,
+      ),
+    ),
+    onPressed: onPressed,
+    color: ManagerColors.primaryColor,
+    height: ManagerHeight.h40,
+  );
+}
