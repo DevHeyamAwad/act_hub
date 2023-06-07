ca8a75e6a24fec03f044b7006e7b8fb75dde60cc
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-08 02:04:43 +0300

    create attribute category response

---
+++ lib/features/home/data/response/home_response.dart
@@ -0,0 +1,8 @@
+// import 'package:act_hub/core/base_response/base_response.dart';
+// import 'package:json_annotation/json_annotation.dart';
+
+// @JsonSerializable()
+
+// class HomeResponse extends BaseResponse{
+//   @JsonKey(name:'sliders')
+// }
