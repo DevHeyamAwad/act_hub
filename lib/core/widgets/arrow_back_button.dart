e837fe3bb030ed736bf0ca3ff709964774fd344d
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-25 02:07:31 +0300

    create arrow back button

---
+++ lib/core/widgets/arrow_back_button.dart
@@ -0,0 +1,30 @@
+import 'package:act_hub/config/constants.dart';
+import 'package:act_hub/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../resources/manager_colors.dart';
+import '../resources/manager_icons.dart';
+import '../resources/manager_sizes.dart';
+import '../service/icon_service.dart';
+import 'main_button.dart';
+
+Widget arrowBackButton({Color? color}) {
+  return Container(
+    margin: EdgeInsetsDirectional.only(
+      top: ManagerHeight.h12,
+    ),
+    child: mainButton(
+      onPressed: () {
+        Get.offAllNamed(Routes.loginView);
+      },
+      child: IconService().getIcon(
+        icon: ManagerIcons.arrow_back_ios,
+        color: color ?? ManagerColors.white,
+        iconSize: ManagerIconSize.s24,
+      ),
+      color: ManagerColors.transparent,
+      elevation: Constants.arrowBackButtonElevation,
+    ),
+  );
+}
